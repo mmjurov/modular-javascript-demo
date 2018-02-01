55037dfc4942e7bf6e74bdf1858819e7e10d8486
Журов Михаил <m.zhurov@maximaster.ru>
2018-02-01 23:44:16 +0300

    Контроль за состоянием

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
+var throbblerShowed = false;
 function showThrobbler() {
     var throbbler = initThrobbler();
     throbbler.show();
+    throbblerShowed = true;
 }
 
 function hideThrobbler() {
     var throbbler = initThrobbler();
     throbbler.hide();
+    throbblerShowed = false;
 }
 
 function initThrobbler() {
@@ -24,6 +27,26 @@ function initThrobbler() {
     return throbbler;
 }
 
+var $startButton = $('#js-start-app');
+var $stopButton = $('#js-stop-app');
 
-$('#js-start-app').on('click', showThrobbler);
-$('#js-stop-app').on('click', hideThrobbler);
+var switchButtonsState = function() {
+    if (throbblerShowed) {
+        $startButton.attr('disabled', true);
+        $stopButton.attr('disabled', false);
+    } else {
+        $startButton.attr('disabled', false);
+        $stopButton.attr('disabled', true);
+    }
+};
+switchButtonsState();
+
+$startButton.on('click', function() {
+    showThrobbler();
+    switchButtonsState();
+});
+
+$stopButton.on('click', function () {
+    hideThrobbler();
+    switchButtonsState();
+});
