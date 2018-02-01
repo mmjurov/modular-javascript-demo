8897a100ff951059a633864f0e91dc02bab6f68f
Журов Михаил <m.zhurov@maximaster.ru>
2018-02-02 00:09:51 +0300

    Module pattern

--- js/app.js
+++ js/app.js
@@ -1,18 +1,103 @@
 var application = {};
 
-loader.provide('application');
-loader.require('application.controller');
+application.throbbler = (function() {
 
-application.start = function() {
-    application.$startButton.on('click', function() {
-        application.showThrobbler();
-        application.switchButtonsState();
-    });
+    var currentFile,
+        throbbler = $('#js-throbbler'),
+        showed = false;
 
-    application.$stopButton.on('click', function () {
-        application.hideThrobbler();
-        application.switchButtonsState();
-    });
-};
+    var show = function() {
+        init().show();
+        showed = true;
+    };
 
-application.start();
+    var hide = function() {
+        init().hide();
+        showed = false;
+    };
+
+    var getRandomDigit = function() {
+        return ~~(Math.random() * 10);
+    };
+
+    var getFilePath = function(digit) {
+        if (!parseInt(digit)) {
+            digit = getRandomDigit();
+        }
+
+        return './img/loaders/' + digit + '.svg';
+    };
+
+    var regenerateFile = function() {
+        currentFile = getFilePath();
+        return currentFile;
+    };
+    regenerateFile();
+
+    var getCssBackground = function() {
+        return 'url(' + currentFile + ') no-repeat center center';
+    };
+
+    var getCurrentFile = function() {
+        return currentFile;
+    };
+
+    var isShowed = function() {
+        return showed === true;
+    };
+
+    var init = function() {
+
+        if (throbbler.length === 0) {
+            throbbler = $('<div id="js-throbbler" class="throbbler"></div>')
+                .css('background', getCssBackground());
+            $('.throbbler-container').append(throbbler);
+        } else {
+            regenerateFile();
+            throbbler.css('background', getCssBackground());
+        }
+
+        return throbbler;
+    };
+
+    return {
+        show: show,
+        hide: hide,
+        getCurrentFile: getCurrentFile,
+        isShowed: isShowed
+    }
+})();
+
+application.controller = (function(throbbler) {
+    var $start = $('#js-start-app');
+    var $stop = $('#js-stop-app');
+
+    var redraw = function() {
+        if (throbbler.isShowed()) {
+            $start.attr('disabled', true);
+            $stop.attr('disabled', false);
+        } else {
+            $start.attr('disabled', false);
+            $stop.attr('disabled', true);
+        }
+    };
+
+    redraw();
+
+    var init = function () {
+        $start.on('click', function() {
+            throbbler.show();
+            redraw();
+        });
+        $stop.on('click', function() {
+            throbbler.hide();
+            redraw();
+        });
+    };
+
+    return {
+        init: init
+    }
+})(application.throbbler);
+
+application.controller.init();
